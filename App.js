ab29cd895eff7cbd1a839e02d8bd7412e21dba6c
Richard <Hounslow>
2019-03-20 11:49:13 +1100

    Messing w/ Stylesheet API and Flexbox

--- App.js
+++ App.js
@@ -6,35 +6,35 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, TextInput } from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  state = { placename: '' };
+  state = { placename: "" };
 
-
-  placeNameChangeHandler = (event) => {
+  placeNameChangeHandler = event => {
     // note; *this in hear refers to the App Component in this () type of method
     // alert(event)
 
     this.setState({
       placename: event
-    })
+    });
   };
 
   render() {
     return (
+
       <View style={styles.container}>
         <TextInput
-          style={{width: 300, borderColor: "black", borderWidth: 1}}
+          style={{ width: 300, borderColor: "black", borderWidth: 1 }}
           value={this.state.placename}
           onChangeText={this.placeNameChangeHandler}
         />
@@ -43,21 +43,15 @@ export default class App extends Component<Props> {
   }
 }
 
+// https://facebook.github.io/react-native/docs/stylesheet
 const styles = StyleSheet.create({
+  // https://facebook.github.io/react-native/docs/flexbox
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    // padding: 10,
+    marginTop: 10,
+    justifyContent: "flex-start", // flex- direction default - top-to-bottom - white background fix w/ flex: 1
+    alignItems: "center",
+    backgroundColor: "#fff"
+  }
 });
