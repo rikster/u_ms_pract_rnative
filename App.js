3b2290f3dcd5ce8275066940f46db86a5bdde594
Richard <Hounslow>
2019-03-20 15:51:36 +1100

    Adding a button and updating state on the screen

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from "react";
 import {
   Platform,
@@ -16,48 +8,60 @@ import {
   Button
 } from "react-native";
 
-type Props = {};
 export default class App extends Component<Props> {
-  state = { placename: "" };
+  state = { placename: "", places: [] };
 
   placeNameChangeHandler = event => {
-    // note; *this in hear refers to the App Component in this () type of method
-    // alert(event)
-
     this.setState({
       placename: event
     });
   };
 
+  placeSubmitHandler = () => {
+    if (this.state.placename === "") {
+      return;
+    }
+
+    // state update that deals w/ async
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placename)
+      };
+    });
+  };
+
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
+
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer} >
+        <View style={styles.inputContainer}>
           <TextInput
             style={styles.placeInput}
             placeholder="a place holder"
-            style={{ }}
+            style={{}}
             value={this.state.placename}
             onChangeText={this.placeNameChangeHandler}
           />
-          <Button title="Add" style={styles.buttonInput} />
+          <Button
+            title="Add"
+            style={styles.buttonInput}
+            onPress={this.placeSubmitHandler}
+          />
         </View>
+        <View>{placesOutput}</View>
       </View>
     );
   }
 }
 
-// API (not real CSS):
-// https://facebook.github.io/react-native/docs/stylesheet
-// Style cheatsheet:
-// https://github.com/vhpoet/react-native-styling-cheat-sheet
 const styles = StyleSheet.create({
-  // https://facebook.github.io/react-native/docs/flexbox
   container: {
     flex: 1,
     padding: 10,
-    // marginTop: 10,
-    justifyContent: "flex-start", // flex- direction default - top-to-bottom - white background fix w/ flex: 1
+    justifyContent: "flex-start",
     alignItems: "center",
     backgroundColor: "#fff"
   },
@@ -71,7 +75,7 @@ const styles = StyleSheet.create({
   placeInput: {
     width: "70%"
   },
-  buttonInput:{
+  buttonInput: {
     width: "30%"
   }
 });
